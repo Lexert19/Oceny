6d3999e9b584a00540f7672083f15cf4753acc84
Lexert19 <dominikch19@gmail.com>
2023-11-29 19:40:15 +0100

    nwm

--- app/src/main/java/com/example/oceny/listener/IsGradesButtonActive.java
+++ app/src/main/java/com/example/oceny/listener/IsGradesButtonActive.java
@@ -15,26 +15,6 @@ public class IsGradesButtonActive implements TextWatcher {
     }
 
 
-
-    /*@Override
-    public boolean onKey(View view, int i, KeyEvent keyEvent) {
-        int gradesNumber =0;
-        try {
-            gradesNumber = Integer.parseInt(context.getGradesNumberInput().getText().toString());
-        }catch (Exception e){}
-
-        context.getGradesButton().setEnabled(false);
-        if(!(gradesNumber >= 5 && gradesNumber <= 15))
-            return false;
-        if(context.getSurnameInput().getText().toString().length() == 0)
-            return false;
-        if(context.getNameInput().getText().toString().length() == 0)
-            return false;
-
-        context.getGradesButton().setEnabled(true);
-        return true;
-    }*/
-
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
